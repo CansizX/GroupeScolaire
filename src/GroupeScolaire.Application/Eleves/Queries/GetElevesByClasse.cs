686510b48c32ec5e0d5191e0c73732fd48fcf169
MSI <MSI@DESKTOP-A8T1D76>
2026-07-26 21:24:50 -0400

    feat: add GetElevesByClasse query
    - Add GetElevesByClasseQuery/Handler with EleveDto
    - Add GET endpoint in ElevesController
    - Validate end-to-end query flow

---
+++ src/GroupeScolaire.Application/Eleves/Queries/GetElevesByClasse.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace GroupeScolaire.Application.Eleves.Queries.GetElevesByClasse;
+
+public record GetElevesByClasseQuery(Guid ClasseId) : IRequest<List<EleveDto>>;
+
+public record EleveDto(Guid Id, string Nom, string Prenom, DateOnly DateNaissance);
---
+++ src/GroupeScolaire.Application/Eleves/Queries/GetElevesByClasseHandler.cs
@@ -0,0 +1,23 @@
+﻿using GroupeScolaire.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace GroupeScolaire.Application.Eleves.Queries.GetElevesByClasse;
+
+public class GetElevesByClasseHandler : IRequestHandler<GetElevesByClasseQuery, List<EleveDto>>
+{
+    private readonly IEtablissementDbContext _context;
+
+    public GetElevesByClasseHandler(IEtablissementDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<EleveDto>> Handle(GetElevesByClasseQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Eleves
+            .Where(e => e.ClasseId == request.ClasseId)
+            .Select(e => new EleveDto(e.Id, e.Nom, e.Prenom, e.DateNaissance))
+            .ToListAsync(cancellationToken);
+    }
+}
