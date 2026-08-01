b77ed1f11d9ffa301a65200b1cac941dd49c2333
MSI <MSI@DESKTOP-A8T1D76>
2026-08-01 15:36:48 -0400

    test: add xUnit test project with first CreateEleveHandler tests
    - Setup xUnit + Moq + EF InMemory + FluentAssertions
    - Add TestEtablissementDbContext for isolated in-memory testing
    - Add Fact test validating Eleve creation
    - Add Theory test revealing missing validation on empty name fields

---
+++ tests/GroupeScolaire.Application.Tests/Common/TestEtablissementDbContext.cs
@@ -0,0 +1,26 @@
+﻿using GroupeScolaire.Application.Common.Interfaces;
+using GroupeScolaire.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GroupeScolaire.Application.Tests.Common;
+
+public class TestEtablissementDbContext : DbContext, IEtablissementDbContext
+{
+    public TestEtablissementDbContext(DbContextOptions<TestEtablissementDbContext> options) : base(options) { }
+
+    public DbSet<Eleve> Eleves => Set<Eleve>();
+    public DbSet<Staff> Staffs => Set<Staff>();
+    public DbSet<Presence> Presences => Set<Presence>();
+}
+
+public static class TestDbContextFactory
+{
+    public static TestEtablissementDbContext Create()
+    {
+        var options = new DbContextOptionsBuilder<TestEtablissementDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+        return new TestEtablissementDbContext(options);
+    }
+}
